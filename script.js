ef25b863f583f7e46c42323dce90c336e358c3f8
Derek <nightwing917@hotmail.com>
2021-07-29 19:53:21 -0700

    added results array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-
+console.log('Connected!');
 
 // NASA API
 const count = 10;
 const apiKey = 'DEMO_KEY';
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`;
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
