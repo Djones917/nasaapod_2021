60bf2cc0dd71a4f74c71a3a72932d654b5e1fab3
Derek <nightwing917@hotmail.com>
2021-07-30 19:35:23 -0700

    adding get nasa pictures function

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+
+// Ge 10 Images from NASA API
+async function getNasaPictures() {
+    try {
+      const response = await fetch(apiUrl);
+    } catch (error) {
+    // Catch error here
+
+    }
+}
